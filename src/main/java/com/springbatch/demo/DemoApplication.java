e33a65d8a1a28d21d06c8c3ca5479d8cf81c3fc4
Yakusugi <yosukeito1987@gmail.com>
2023-08-23 21:54:34 +0900

    second commit

--- src/main/java/com/springbatch/demo/DemoApplication.java
+++ src/main/java/com/springbatch/demo/DemoApplication.java
@@ -1,10 +1,49 @@
 package com.springbatch.demo;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class DemoApplication {
+	
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step firstStep() {
+		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("step1 executed");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	
+		
+	}
+	
+	@Bean
+	public Job firstJob() {
+		return this.jobBuilderFactory.get("job1")
+				.start(firstStep())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
