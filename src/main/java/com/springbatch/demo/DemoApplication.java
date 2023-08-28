63d9d0b443c6f88abc7110338171643c770ad312
Yakusugi <yosukeito1987@gmail.com>
2023-08-28 09:10:50 +0900

    commit august 28

--- src/main/java/com/springbatch/demo/DemoApplication.java
+++ src/main/java/com/springbatch/demo/DemoApplication.java
@@ -6,48 +6,57 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.springbatch.tasklet.MyTasklet;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@ComponentScan(basePackages = "com.springbatch") 
 public class DemoApplication {
-	
-	@Autowired
-	public JobBuilderFactory jobBuilderFactory;
-	
-	@Autowired
-	public StepBuilderFactory stepBuilderFactory;
-	
-	@Bean
-	public Step firstStep() {
-		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
-			
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step1 executed");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
-	
-		
-	}
-	
-	@Bean
-	public Job firstJob() {
-		return this.jobBuilderFactory.get("job1")
-				.start(firstStep())
-				.build();
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("This is a test");
-	}
+//    @Autowired
+//    private JobBuilderFactory jobBuilderFactory;
+//
+//    @Autowired
+//    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+    
+    @Bean
+    public Tasklet myTasklet() {
+        return new MyTasklet();
+    }
+
+    @Bean
+    public Job myJob(JobRepository jobRepository, Step step) {
+    	return new JobBuilder("myJob", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step firstStep(JobRepository jobRepository, Tasklet myTasklet, PlatformTransactionManager transactionManager) {
+    	return new StepBuilder("myJob", jobRepository)
+    			.tasklet(myTasklet, transactionManager)
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("This is a test");
+    }
 
 }
